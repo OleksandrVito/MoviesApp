b14b2de5c6a5903cf86f2132f3180bd1130dd692
Oleksandr <vito.oleksandr@gmail.com>
2023-01-25 21:32:19 +0200

    Lesson-2

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -13,14 +13,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.moviesapp.navigation.SetupNavHost
 import com.example.moviesapp.ui.theme.MoviesAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesAppTheme {
               val navController = rememberNavController()
-                SetupNavHost(navController = navController)
+              SetupNavHost(navController = navController)
             }
         }
     }
