8a01a51ee319782d29dc1290dfa4578fcfbe2777
Oleksandr <vito.oleksandr@gmail.com>
2023-01-25 13:45:59 +0200

    Lesson-1

---
+++ app/src/main/java/com/example/moviesapp/screens/SplashScreen.kt
@@ -0,0 +1,61 @@
+package com.example.moviesapp.screens
+
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.moviesapp.navigation.Screens
+import com.example.moviesapp.ui.theme.MoviesAppTheme
+import com.example.moviesapp.utils.Constants
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    var startAnimated by remember {
+        mutableStateOf(false)
+    }
+    val alphaAnimation = animateFloatAsState(
+        targetValue = if (startAnimated) 1f else 0f,
+        animationSpec = tween(durationMillis = 5000)
+    )
+    LaunchedEffect(key1 = true) {
+        startAnimated = true
+        delay(
+            4000
+        )
+        navController.navigate(Screens.Main.route)
+    }
+
+    Splash(alpha = alphaAnimation.value)
+}
+
+@Composable
+fun Splash(alpha: Float) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(alpha = alpha),
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            imageVector = Icons.Default.PlayArrow,
+            contentDescription = "",
+            tint = Color.Black
+        )
+    }
+}
+
